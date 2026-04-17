2ca431e4177e9e647f689518ab128ec71b22bcf8
Diego <die.gutierrezi@duocuc.cl>
2026-04-17 19:34:30 -0400

    Se crea el proyecto

---
+++ src/main/java/com/locafy/locafy_backend/LocafyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.locafy.locafy_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocafyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocafyBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/locafy/locafy_backend/LocafyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.locafy.locafy_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocafyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
